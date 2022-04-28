454596a2277a34bff30e09e325353ef90a626ddb
Noah Reece <noah.w.reece@gmail.com>
2022-04-28 11:13:43 -0400

    commit tree 677566738503390941706cd2c95e6b94995da0cb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  # error handling for non-existent records and failed validations
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  private
+
+  # response when requested record not in database
+  def render_not_found_response
+    render json: { error: 'Not found' }, status: :not_found
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+  before_action :find_items
+  before_action :find_item, only: %i[show]
 
+  # GET /items || GET /users/:user_id/items
   def index
-    items = Item.all
-    render json: items, include: :user
+    render json: @items, include: :user
   end
 
+  # GET /users/:user_id/items/:id
+  def show
+    render json: @item
+  end
+
+  # POST /users/:user_id/items
+  def create
+    new_item = @items.create!(item_params)
+    render json: new_item, status: :created
+  end
+
+  private
+
+  # set instance variable for show/update/destroy
+  def find_item
+    @item = @items.find(params[:id])
+  end
+
+  def find_items
+    if (params[:user_id])
+      @items = User.find(params[:user_id]).items
+    else
+      @items = Item.all
+    end
+  end
+
+  # permissible params to be used by create/update
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
